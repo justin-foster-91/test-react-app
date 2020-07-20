9aa9da48eb26cb060761e027a6445195e681d6bd
Tiffany <tiffanysummerford@gmail.com>
2020-07-20 12:47:11 -0400

    basic wireframe setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 function App() {
   return (
+    <>
+    {
+      (
+      <header>
+      <div id="img">
+        <img src='./src/sally.jpg' />
+      </div>
+      <div>
+      <h1 class="title">About me</h1>
+      </div>
+    </header>
+      )
+    }
     <main className='App'>
+  <section>{}</section>
       {/* content goes here */}
     </main>
+    </>
   );
 }
 export default App;
